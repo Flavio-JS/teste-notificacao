29408986ff2e0dbe59eb9e3a61c52edfaa771037
Flavio Filho <flavio.filho@trinusco.com.br>
2023-04-28 14:59:12 -0300

    removido comentário

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,35 +42,3 @@ const PermissaoPage: NextPage = () => {
 };
 
 export default PermissaoPage;
-
-// import { NextPage } from "next";
-// import { useEffect } from "react";
-
-// const PermissaoPage: NextPage = () => {
-//   const requestPermission = async () => {
-//     await Notification.requestPermission();
-//   };
-
-//   useEffect(() => {
-//     requestPermission();
-//   }, []);
-
-//   const handleNotificationClick = () => {
-//     const notification = new Notification("Exemplo de Notificação", {
-//       body: "Essa é uma notificação de exemplo.",
-//     });
-//     notification.onclick = (event) => {
-//       event.preventDefault();
-//       window.open("https://www.seusite.com.br");
-//     };
-//   };
-
-//   return (
-//     <div>
-//       <button onClick={requestPermission}>Permitir Notificações</button>
-//       <button onClick={handleNotificationClick}>Enviar Notificação</button>
-//     </div>
-//   );
-// };
-
-// export default PermissaoPage;
