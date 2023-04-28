e816c2ac83b947f8e904e0784085d526b7d2081e
Flavio Filho <flavio.filho@trinusco.com.br>
2023-04-28 13:12:31 -0300

    build

--- src/pages/permicao/index.tsx
+++ src/pages/permicao/index.tsx
@@ -1,18 +1,18 @@
 import { NextPage } from "next";
-import { useState } from "react";
+import { useEffect } from "react";
 
-const PermicaoPage: NextPage = () => {
-  const [permission, setPermission] = useState(Notification.permission);
-
+const PermissaoPage: NextPage = () => {
   const requestPermission = async () => {
-    const result = await Notification.requestPermission();
-    setPermission(result);
+    await Notification.requestPermission();
   };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
 
   const handleNotificationClick = () => {
     const notification = new Notification("Exemplo de Notificação", {
       body: "Essa é uma notificação de exemplo.",
-      icon: "/path/to/icon.png",
     });
     notification.onclick = (event) => {
       event.preventDefault();
@@ -20,15 +20,18 @@ const PermicaoPage: NextPage = () => {
     };
   };
 
+  if (typeof window === "undefined") {
+    // retorna null no ambiente de servidor
+    return null;
+  }
+
   return (
     <div>
-      {permission === "granted" ? (
-        <button onClick={handleNotificationClick}>Enviar Notificação</button>
-      ) : (
-        <button onClick={requestPermission}>Permitir Notificações</button>
-      )}
+      <button onClick={handleNotificationClick}>Enviar Notificação</button>
+
+      <button onClick={requestPermission}>Permitir Notificações</button>
     </div>
   );
 };
 
-export default PermicaoPage;
+export default PermissaoPage;
